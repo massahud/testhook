a8586c3451fe2d0b1f6478f320bf28c2c3e984ba
Geraldo Massahud <massahud@gmail.com>
2020-07-28 23:09:05 +0100

    Fixing doc

--- doc.go
+++ doc.go
@@ -6,21 +6,22 @@
 // configure the hooks and call Run on the TestHook.
 //
 // Example:
-// func TestSomething(t *testing.T) {
-// 	th := testhook.Wrap(t)
 //
-// 	th.BeforeEach(func(t *testing.T) {
-// 		fmt.Println(t.Name(), "started")
-// 	})
+//  func TestSomething(t *testing.T) {
+//  	th := testhook.Wrap(t)
 //
-// 	th.AfterEach(func(t *testing.T) {
-// 		fmt.Println(t.Name(), "passed?", !t.Failed())
-// 	})
+//  	th.BeforeEach(func(t *testing.T) {
+//  		fmt.Println(t.Name(), "started")
+//  	})
 //
-// 	th.Run("a subtest", func(t *testing.T) {
-// 		t.Log("foo")
-// 	})
-// }
+//  	th.AfterEach(func(t *testing.T) {
+//  		fmt.Println(t.Name(), "passed?", !t.Failed())
+//  	})
+//
+//  	th.Run("a subtest", func(t *testing.T) {
+//  		t.Log("foo")
+//  	})
+//  }
 //
 // The afterEach hook runs even if the test fails. If you do not want it to run
 // check t.Failed in your hook.
